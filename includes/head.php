d52ad9125a0db5425f12d53d4a7fd18e397a48b4
Satyendra Sharma <satyendrasharmasatya4@gmail.com>
2024-07-01 15:28:55 +0530

    created header

---
+++ includes/head.php
@@ -0,0 +1,12 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Natural Ayurveda Tips: Timeless Wisdom for Holistic Health</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/font.css">
+    <link rel="stylesheet" href="assets/css/swiper.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" id="theme-change" type="text/css" href="#">
+    <link rel="shortcut icon" href="assets/images/fav.png" type="image/x-icon">
+</head>
--- index.php
+++ index.php
@@ -1,1 +1,31 @@
-satya
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once ('includes/head.php'); ?>
+
+<body>
+    <!-- pre loader start -->
+    <div class="pa-preloader">
+        <div class="pa-ellipsis">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+    <!-- pre loader end -->
+    <!-- main wrapper start -->
+    <div class="pa-main-wrapper">
+        <?php include_once ('includes/header.php'); ?>
+
+
+        <!-- yaha se website suru hoga -->
+    </div>
+    <!-- yaha color dalega -->
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/SmoothScroll.min.js"></script>
+    <script src="assets/js/swiper.min.js"></script>
+    <script src="assets/js/custom.js"></script>
+</body>
+
+</html>
