79e39150dc475c4d38c0f4fb1b3962d86d57b18b
Satyendra Sharma <satyendrasharmasatya4@gmail.com>
2024-07-02 18:01:03 +0530

    Update return-policy.php

--- return-policy.php
+++ return-policy.php
@@ -1,3 +1,59 @@
-<center>
-    <h1>Return Policy</h1>
-</center>
+<?php include_once ('includes/head.php'); ?>
+<?php include_once ('includes/header.php'); ?>
+
+<!-- breadcrumb start -->
+<div class="pa-breadcrumb">
+    <div class="container-fluid">
+        <div class="pa-breadcrumb-box">
+            <h1>Return, Replacement & Refund Policy</h1>
+            <ul>
+                <li><a href="./">Home</a></li>
+                <li>Return, Replacement & Refund Policy</li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="pa-about spacer-top spacer-bottom">
+    <div class="container">
+        <h2 class="text-center">Return and Refund Policy</h2>
+
+        <div class="policy-section">
+            <h4>1. Initiate Return/Refund</h4>
+            <p>If you have received an item in a damaged/defective condition or have been sent a wrong product, you can
+                initiate return/refund within 5 (five) days of receiving the order.</p>
+        </div>
+
+        <div class="policy-section">
+            <h4>2. Contact Support</h4>
+            <p>Reach out to our support team with your order details on <a
+                    href="mailto:support@naturalayurvedatips.com">support@naturalayurvedatips.com</a> with the following
+                details:</p>
+            <ul>
+                <li>Order ID details</li>
+                <li>Request to return/replace/refund your order.</li>
+                <li>Kindly email an image of the product and the invoice for our reference.</li>
+            </ul>
+        </div>
+
+        <div class="policy-section">
+            <h4>3. Pickup Arrangement</h4>
+            <p>We will arrange for the pick-up of the products within 2-4 business days. We will initiate the refund or
+                replacement process only if the products are received by us in their original packaging with their
+                seals, labels and barcodes intact.</p>
+        </div>
+
+        <div class="policy-section">
+            <h4>4. Refund Process</h4>
+            <p>Once we receive the product(s) back and verify its packaging/condition, we will refund your money within
+                7-14 business days to your original payment mode.</p>
+        </div>
+
+        <div class="policy-section">
+            <h4>5. Condition of Products</h4>
+            <p>Products will not be accepted if the seal is broken.</p>
+        </div>
+    </div>
+</div>
+
+<?php include_once ('includes/footer.php'); ?>
