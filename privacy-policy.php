92fa4a01e1a7d5d9f707f60d24461549607f9b37
Satyendra Sharma <satyendrasharmasatya4@gmail.com>
2024-07-03 11:49:13 +0530

    added privacy policy page

--- privacy-policy.php
+++ privacy-policy.php
@@ -1,3 +1,80 @@
-<center>
-    <h1>Privacy Policy</h1>
-</center>
+<?php include_once ('includes/head.php'); ?>
+<?php include_once ('includes/header.php'); ?>
+
+<!-- breadcrumb start -->
+<div class="pa-breadcrumb">
+    <div class="container-fluid">
+        <div class="pa-breadcrumb-box">
+            <h1>PRIVACY POLICY</h1>
+            <ul>
+                <li><a href="./">Home</a></li>
+                <li>PRIVACY POLICY</li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="pa-about spacer-top spacer-bottom">
+    <div class="container">
+
+        <h3>Do we share any personal customer information with anyone?</h3>
+        <p>We share your personal information only with few of our trusted partners including the payment gateway,
+            order fulfilment and tracking partners. Sharing of this data is necessary in order to fulfil your order. Be
+            assured that we will do our best to always protect your personal information.</p>
+
+        <h3>What do we do with your information?</h3>
+        <p>When you purchase something from our store, as part of the buying and selling process, we collect the
+            personal information you give us such as your name, address, phone number, email address, billing, order
+            history, feedback, about your visits to and use of naturalayurvedatips.com (including your IP address,
+            geographical location, browser type and version, operating system, referral source, length of visit, page
+            views and website navigation etc.)</p>
+        <p>Email /SMS (if applicable): We may send you emails or SMS about your order, our store, new products, and
+            other updates.</p>
+
+        <h3>Consent</h3>
+        <h4>How do you get my consent?</h4>
+        <p>When you provide us with personal information to complete a transaction, place an order, arrange for a
+            delivery or return a purchase, we imply that you consent to us collecting it and using it for that specific
+            reason only.</p>
+        <p>If we ask for your personal information for a secondary reason, like marketing, we will either ask you
+            directly for your expressed consent or provide you with an opportunity to say no.</p>
+
+        <h4>How do I withdraw my consent?</h4>
+        <p>If after you opt-in, you change your mind, you may withdraw your consent for us to contact you, for the
+            continued collection, use or disclosure of your information, at any time, by contacting us at
+            support@naturalayurvedatips.com</p>
+
+        <h3>Disclosure</h3>
+        <p>We may disclose your personal information if we are required by law to do so or if you violate our Terms of
+            Service.</p>
+
+        <h3>Payment & Security</h3>
+        <p>Sparkeytips.com is committed to provide you with the most secure experience.</p>
+
+        <h3>Information security</h3>
+        <p>To protect your personal information, we take reasonable precautions and follow industry best practices to
+            make sure it is not inappropriately lost, misused, accessed, disclosed, altered or destroyed. Any
+            information that we collect and store that is personally identifiable is protected using appropriate means,
+            and Sparkeytips is not responsible for any unauthorized access or use by third parties.</p>
+        <p>If you provide us with your credit card, net banking, UPI, mobile wallets or any other online payment
+            information, the information is encrypted with the payment gateway providers only and not by us.</p>
+
+        <h3>Age of consent</h3>
+        <p>By using this site, you represent that you are at least the age of majority, or that you are the age of
+            majority you have given us your consent to allow any of your minor dependents to use this site.</p>
+
+        <h3>Changes to this privacy policy</h3>
+        <p>We reserve the right to modify this privacy policy at any time, so please review it frequently. Changes and
+            clarifications will take effect immediately upon their posting on the website. If we make material changes
+            to this policy, we will notify you here that it has been updated, so that you are aware of what information
+            we collect, how we use it, and under what circumstances, if any, we use and/or disclose it.</p>
+        <p>If our store is acquired or merged with another company in future, your information may be transferred to the
+            new owners so that we may continue to sell products to you.</p>
+
+        <h3>Questions and contact information</h3>
+        <p>If you would like to: access, correct, amend or delete any personal information we have about you, register a
+            complaint, or simply want more information, contact our customer support at support@naturalayurvedatips.com</p>
+    </div>
+</div>
+
+<?php include_once ('includes/footer.php'); ?>
